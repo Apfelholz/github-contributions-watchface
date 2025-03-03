a2f02050b6058ad3326d63fcb9652b3a7aecf71f
Apfelholz <apfelholzz1@gmail.com>
2025-03-03 23:32:27 +0000

    fead: Update message keys and implement GitHub contributions fetching functionality

--- github-contributions-watchface/src/c/github-contributions-watchface.c
+++ github-contributions-watchface/src/c/github-contributions-watchface.c
@@ -1,7 +1,30 @@
 #include <pebble.h>
 
+#define KEY_CONTRIBUTIONS 0
+
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static int s_contributions = 0;
+
+static void update_background_color() {
+  GColor background_color;
+
+  if (s_contributions == 0) {
+    background_color = GColorDarkGray;
+  } else if (s_contributions <= 3) {
+    background_color = GColorDarkGreen;
+  } else if (s_contributions <= 5) {
+    background_color = GColorIslamicGreen;
+  } else if (s_contributions <= 7) {
+    background_color = GColorGreen;
+  } else if (s_contributions <= 10) {
+    background_color = GColorBrightGreen;
+  } else {
+    background_color = GColorMintGreen;
+  }
+
+  window_set_background_color(s_main_window, background_color);
+}
 
 static void update_time() {
   time_t temp = time(NULL);
@@ -31,11 +54,40 @@ static void main_window_load(Window *window){
 
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
+  update_background_color();
   update_time();
 }
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  Tuple *t = dict_read_first(iterator);
+  while (t != NULL) {
+    switch (t->key) {
+      case KEY_CONTRIBUTIONS:
+        s_contributions = t->value->int32;
+        update_background_color();
+        break;
+      default:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+        break;
+    }
+    t = dict_read_next(iterator);
+  }
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+} 
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
 static void init() {
@@ -47,6 +99,16 @@ static void init() {
   });
   window_stack_push(s_main_window, true);
   update_time();
+
+  app_message_register_inbox_received(inbox_received_callback);
+  
+  const int inbox_size = 128;
+  const int outbox_size = 128;
+  app_message_open(inbox_size, outbox_size);
+
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
 }
 
 static void deinit() {
