3301c3dcf4a2d18b825cee769c71817899a5c7e8
Apfelholz <apfelholzz1@gmail.com>
2025-03-09 19:49:08 +0000

    fix: Refactor contributions data structure to a flat array and update fetching logic

--- github-contributions-watchface/src/pkjs/index.js
+++ github-contributions-watchface/src/pkjs/index.js
@@ -11,19 +11,19 @@ Pebble.addEventListener('appmessage', function(e) {
 
 function sendContributions() {
   const contributions = [
-    [0, 1, 2, 3, 4, 5, 6],
-    [7, 8, 9, 10, 11, 12, 13],
-    [14, 15, 16, 17, 18, 19, 20],
-    [21, 22, 23, 24, 25, 26, 27],
-    [28, 29, 30, 31, 32, 33, 34],
-    [35, 36, 37, 38, 39, 40, 41],
-    [42, 43, 44, 45, 46, 47, 48]
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 
+    40, 41, 42, 43, 44, 45, 46, 47, 48
   ];
 
-  
+  var buffer = new ArrayBuffer(contributions.length);
+  var view = new Uint8Array(buffer);
+  for (var i = 0; i < contributions.length; i++) {
+    view[i] = contributions[i];
+  }
 
   var dict = {
-    'KEY_CONTRIBUTIONS': contributions[6][6]
+    'KEY_CONTRIBUTIONS': Array.prototype.slice.call(view)
   };
 
   Pebble.sendAppMessage(dict, function() {
